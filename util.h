afc0e47bd5b2d55518b8b3f61002acf1ee9e4309
local_ubuntu <wenqvip@gmail.com>
2020-03-15 17:28:43 +0800

    1

--- util.h
+++ util.h
@@ -4,5 +4,8 @@ typedef unsigned char uchar;
 typedef unsigned short ushort;
 typedef unsigned int uint;
 typedef unsigned long ulong;
+#if defined(WIN64) || defined(WIN32)
+typedef long ssize_t;
+#endif
 
 std::string str_to_hex(const char* str, int len);
